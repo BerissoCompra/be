c3cb2ca69e80bbc6498e6f638d0492358fa365ed
CiudadAPP <96387703+BerissoCompra@users.noreply.github.com>
2022-03-21 12:52:18 -0300

    Delete build directory

--- build/models/enum/tipo-filtro.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TipoFiltroEnum = void 0;
-var TipoFiltroEnum;
-(function (TipoFiltroEnum) {
-    TipoFiltroEnum["DESTACADOS"] = "destacados";
-    TipoFiltroEnum["ABIERTOS"] = "abiertos";
-    TipoFiltroEnum["CATEGORIAS"] = "categorias";
-})(TipoFiltroEnum = exports.TipoFiltroEnum || (exports.TipoFiltroEnum = {}));
