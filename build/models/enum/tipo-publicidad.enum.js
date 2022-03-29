d2aba44568d2c11c02574146584a9e920998554e
DESKTOP-J1B1JIN\MCDsoftwares <berissocompratest@gmail.com>
2022-03-29 00:14:19 -0300

    fix2

--- build/models/enum/tipo-publicidad.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TiposPublicidadEnum = void 0;
-var TiposPublicidadEnum;
-(function (TiposPublicidadEnum) {
-    TiposPublicidadEnum["PRINCIPAL"] = "principal";
-    TiposPublicidadEnum["SECCIONAL"] = "seccional";
-})(TiposPublicidadEnum = exports.TiposPublicidadEnum || (exports.TiposPublicidadEnum = {}));
