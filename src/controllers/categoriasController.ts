c1b75827061f9dc017b63c5ffcf034adcf0140e0
Agustin <agustinsurila@gmail.com>
2022-04-07 19:36:09 -0300

    Agregue try/catch al controlador

--- src/controllers/categoriasController.ts
+++ src/controllers/categoriasController.ts
@@ -1,76 +1,105 @@
-
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import CategoriaModel from '../models/Categorias';
 
+class CategoriasController {
+  public async crearCategoria(req: Request, res: Response) {
+    const { descripcion, tipo } = req.body;
+    try {
+      const categoria = new CategoriaModel({
+        descripcion,
+        tipo,
+      });
 
-class CategoriasController{
-
-    public async crearCategoria(req: Request, res: Response){
-        const {body} = req;
-        if(body?.descripcion && body?.tipo){
-            const categoria = new CategoriaModel(body);
-            const action = await categoria.save();
-            if(action){
-                return res.status(201).json({msg: 'Creado correctamente'});
-            }
-            else{
-                return res.status(201).json({msg: 'No se pudo crear'});
-            }
-        }
-        else{
-            return res.status(404).json({msg: 'Debe completar todos los campos'});
-        }
-   
+      const action = await categoria.save();
+      if (action) {
+        return res.status(201).json({ msg: 'Creado correctamente' });
+      } else {
+        return res.status(201).json({ msg: 'No se pudo crear' });
+      }
+    } catch (error) {
+      return res.status(404).json({ msg: 'Debe completar todos los campos' });
     }
+  }
 
-    public async getCategorias(req: Request, res: Response){
-        await CategoriaModel.find({}).exec((err, categorias)=>{
-            if(err)  return res.status(500).json({msg: 'Error al obtener'})
-            return res.status(200).json(categorias)
-        }); 
-        
+  public async getCategorias(req: Request, res: Response) {
+    await CategoriaModel.find({}).exec((err, categorias) => {
+      if (err) return res.status(500).json({ msg: 'Error al obtener' });
+      return res.status(200).json(categorias);
+    });
+  }
+
+  public async getCategoriasPorTipo(req: Request, res: Response) {
+    const { tipo } = req.params;
+    try {
+      const categorias = await CategoriaModel.find({ tipo });
+      if (categorias.includes(tipo)) {
+        return res.status(200).json(categorias);
+      } else {
+        return res.status(200).json('La categoría no existe');
+      }
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ msg: 'Error al obtener las categorias', error });
     }
+  }
 
-    public async getCategoriasPorTipo(req: Request, res: Response){
-        const {tipo} = req.params;
-        await CategoriaModel.find({tipo}).exec((err, categorias)=>{
-            if(err)  return res.status(500).json({msg: 'Error al obtener'})
-            return res.status(200).json(categorias)
-        }); 
+  public async getCategoriasById(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const categoria = await CategoriaModel.findById(id);
+
+      if (!categoria) {
+        return res.status(404).json({ msg: 'No se ha encontrado' });
+      }
+
+      return res.status(200).json(categoria);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ msg: 'Error al obtener las categorias', error });
     }
+  }
 
-    public async getCategoriasById(req: Request, res: Response){
-        const {id} = req.params;
-        await CategoriaModel.findById(id).exec((err, categoria)=>{
-            if(err)  return res.status(500).json({msg: 'Error al obtener'})
-            if(!categoria) return res.status(404).json({msg: 'No se ha encontrado'})
-            return res.status(200).json(categoria)
-        }); 
-        
-    }
-
-    public async updateCategoria(req: Request, res: Response){
-        const {id} = req.params;
-        const {body} = req;
-        const categoria = await CategoriaModel.findByIdAndUpdate(id, body);
-        if(categoria){
-            return res.status(201).json({msg: 'Actualizado correctamente'});
+  public async updateCategoria(req: Request, res: Response) {
+    const { id } = req.params;
+    const { body } = req;
+    console.log(body);
+    try {
+      console.log(body);
+      if (!body.hasOwnProperty('descripcion') || !body.hasOwnProperty('tipo')) {
+        return res
+          .status(404)
+          .json({ msg: 'Se deben ingresar las 2 propiedades para actualizar' });
+      } else {
+        const categoriaUpdated = await CategoriaModel.findByIdAndUpdate(
+          id,
+          body,
+        );
+        if (!categoriaUpdated) {
+          return res.status(201).json({ msg: 'No se pudo actualizar' });
+        } else {
+          return res.status(201).json({ msg: 'Actualizado correctamente' });
         }
-        else{
-            return res.status(201).json({msg: 'No se pudo actualizar'});
-        }
+      }
+    } catch (error) {
+      return res.status(400).json({ msg: 'Hubo un error', error });
     }
+  }
 
-    public async deleteCategoria(req: Request, res: Response){
-        const {id} = req.params;
-        const categoria = await CategoriaModel.findByIdAndDelete(id);
-        if(categoria){
-            return res.status(201).json({msg: 'Eliminado correctamente'});
-        }
-        else{
-            return res.status(201).json({msg: 'No se pudo eliminar'});
-        }
+  public async deleteCategoria(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const categoria = await CategoriaModel.findByIdAndDelete(id);
+      if (categoria) {
+        return res.status(201).json({ msg: 'Eliminado correctamente' });
+      } else {
+        return res.status(201).json({ msg: 'No se pudo eliminar' });
+      }
+    } catch (error) {
+      return res.status(404).json({ msg: 'Hubo un error al eliminar', error });
     }
+  }
 }
 
 export const categoriasController = new CategoriasController();
